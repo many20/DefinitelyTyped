70c390698e7f50f6625ed9ef369db0cbd388a793
Mario Adrian <many.mario@gmx.de>
2018-09-17 12:05:16 +0200

    update

--- types/debounce/index.d.ts
+++ types/debounce/index.d.ts
@@ -1,7 +1,7 @@
-// Type definitions for debounce 1.0
+// Type definitions for debounce 1.2
 // Project: https://github.com/component/debounce
-// Definitions by: Denis Sokolov <https://github.com/denis-sokolov>
+// Definitions by: Denis Sokolov <https://github.com/denis-sokolov>, Mario Adrian <https://github.com/many20>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function debounce<A extends Function>(f: A, interval?: number, immediate?: boolean): A & { clear(): void; };
+declare function debounce<A extends Function>(fn: A, wait?: number, immediate?: boolean): A & { clear(): void; flush(): void; };
 export = debounce;
