fe50a6bbad0962b63457479fb182e3dd7b422992
Mario Adrian <many.mario@gmx.de>
2018-09-17 11:13:38 +0200

    support v4.0

--- types/odata/index.d.ts
+++ types/odata/index.d.ts
@@ -1,83 +1,85 @@
-// Type definitions for odata v0.3
-// Project: https://github.com/janhommes/odata
-// Definitions by: Jan Hommes <https://github.com/janhommes>, Jean-Christophe Chalte <https://github.com/jcchalte>
+// Type definitions for odata v0.4
+// Project: https://github.com/janhommes/o.js
+// Definitions by: Jan Hommes <https://github.com/janhommes>, Jean-Christophe Chalte <https://github.com/jcchalte>, Mario Adrian <Https://github.com/many20>/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.4
 
 declare module 'odata' {
-    import Q = require("q");
+    //import Q = require("q");
 
     interface Options {
-        endpoint : string
-        format ?: string
-        autoFormat ?: boolean
-        version ?: number
-        strictMode ?: boolean
-        start ?: () => any
-        ready ?: () => any
-        error ?: () => any
-        headers ?: { name: string, value: string }[]
-        username ?: string
-        password ?: string
-        isAsync ?: boolean
-        isCors ?: boolean
-        isHashRoute ?: boolean
-        appending ?: string
+        endpoint: string | null;
+        format?: string;
+        autoFormat?: boolean;
+        version?: number;
+        strictMode?: boolean;
+        start?: (() => any) | null;
+        ready?: (() => any) | null;
+        error?: (() => any) | null;
+        headers?: { name: string; value: string }[];
+        username?: string | null;
+        password?: string | null;
+        isAsync?: boolean;
+        isWithCredentials?: boolean;
+        isHashRoute?: boolean;
+        appending?: string;
     }
 
     interface OHandler<T> {
-        inlinecount: number;        // if inlinecount is set, here the counting is gold
-        data: T;                    // holds the data after an callback
-        param: {};				    // this object holds all parameter for a route
+        inlinecount: number; // if inlinecount is set, here the counting is gold
+        data: T; // holds the data after an callback
+        param: {}; // this object holds all parameter for a route
 
-        config<T>(options ?: Options) : OHandler<T>
-        progress<T>(callback : () => any) : OHandler<T>
+        config<T>(options?: Options): OHandler<T>;
+        progress<T>(callback: () => any): OHandler<T>;
 
-        get<T>(callback ?: (data : T) => void, errorCallback?: (status: number) => void) : Q.Promise<OHandler<T>>
-        save<T>(callback ?: (data : T) => void, errorCallback?: (status: number) => void) : Q.Promise<OHandler<T>>
+        get<T>(callback?: (data: T) => void, errorCallback?: (status: number) => void): Q.Promise<OHandler<T>>;
+        save<T>(callback?: (data: T) => void, errorCallback?: (status: number) => void): Q.Promise<OHandler<T>>;
         query: () => string;
+        raw: any;
+        xhr: any; //XMLHttpRequest | Xhr2
 
-        post<T>(params : any) : OHandler<T>
-        patch<T>(params : any) : OHandler<T>
-        put<T>(params : any) : OHandler<T>
-        remove<T>(params ?: any) : OHandler<T>
+        post<T>(params: any): OHandler<T>;
+        patch<T>(params: any): OHandler<T>;
+        put<T>(params: any): OHandler<T>;
+        remove<T>(params?: any): OHandler<T>;
 
-        routes<T>(path : string, callback ?: (data : T) => void) : OHandler<T>
-        route<T>(path : string, callback ?: (data : T) => void) : OHandler<T>
-        triggerRoute(hash : string) : OHandler<T>
-        beforeRouting(callback : (routeParams : any) => boolean) : OHandler<T>
+        routes<T>(path: string, callback?: (data: T) => void): OHandler<T>;
+        route<T>(path: string, callback?: (data: T) => void): OHandler<T>;
+        triggerRoute(hash: string): OHandler<T>;
+        beforeRouting(callback: (routeParams: any) => boolean): OHandler<T>;
 
-        isEndpoint() : boolean
-        loading<T>(startFn : () => any | boolean, endFn : () => any) : OHandler<T>
+        isEndpoint(): boolean;
+        loading<T>(startFn: () => any | boolean, endFn: () => any): OHandler<T>;
 
-        find<T>(selector : string|number) : OHandler<T>
+        find<T>(selector: string | number): OHandler<T>;
 
-        top<T>(quantity : number) : OHandler<T>
-        take<T>(quantity : number) : OHandler<T>
-        skip<T>(quantity : number) : OHandler<T>
-        first<T>() : OHandler<T>
+        top<T>(quantity: number): OHandler<T>;
+        take<T>(quantity: number): OHandler<T>;
+        skip<T>(quantity: number): OHandler<T>;
+        first<T>(): OHandler<T>;
 
-        include<T>(column : string, data : string) : OHandler<T>
-        exclude<T>(column : string, data : string) : OHandler<T>
-        filterByList<T>(column : string, data : string) : OHandler<T>
+        include<T>(column: string, data: string): OHandler<T>;
+        exclude<T>(column: string, data: string): OHandler<T>;
+        filterByList<T>(column: string, data: string): OHandler<T>;
 
-        filter<T>(filter : string) : OHandler<T>
-        where<T>(filter : string) : OHandler<T>
-        any<T>(filter : string, resource : string) : OHandler<T>
-        search<T>(columns : string[], term : string) : OHandler<T>
+        filter<T>(filter: string): OHandler<T>;
+        where<T>(filter: string): OHandler<T>;
+        any<T>(filter: string, resource: string): OHandler<T>;
+        search<T>(columns: string[], term: string): OHandler<T>;
 
-        orderBy<T>(column : string, direction ?: boolean) : OHandler<T>
-        orderByDesc<T>(column : string) : OHandler<T>
-        select<T>(selectStr : string) : OHandler<T>
+        orderBy<T>(column: string, direction?: boolean): OHandler<T>;
+        orderByDesc<T>(column: string): OHandler<T>;
+        select<T>(selectStr: string): OHandler<T>;
 
-        count<T>() : OHandler<T>
-        inlineCount<T>(paramName ?: string) : OHandler<T>
+        count<T>(): OHandler<T>;
+        inlineCount<T>(paramName?: string): OHandler<T>;
 
-        batch<T>(resource : string) : OHandler<T>
-        expand<T>(resource : string) : OHandler<T>
-        ref<T>(resource : string, id : string | number) : OHandler<T>
-        removeRef<T>(resource : string, id : string | number) : OHandler<T>
-        deleteRef<T>(resource : string, id : string | number) : OHandler<T>
+        batch<T>(resource: string): OHandler<T>;
+        expand<T>(resource: string): OHandler<T>;
+        ref<T>(resource: string, id: string | number): OHandler<T>;
+        removeRef<T>(resource: string, id: string | number): OHandler<T>;
+        deleteRef<T>(resource: string, id: string | number): OHandler<T>;
     }
 
     interface OHandlerStatic {
@@ -87,7 +89,6 @@ declare module 'odata' {
         isEndpoint: () => boolean;
     }
 
-    var o : OHandlerStatic;
-
-    export = o
+    const o: OHandlerStatic;
+    export = o;
 }
